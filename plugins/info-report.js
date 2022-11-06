a068ed11b56196a1d1558d0fb089578f36693662
malgtps12 <93052305+malgtps12@users.noreply.github.com>
2022-11-06 15:23:50 +0700

    Update info-report.js

--- plugins/info-report.js
+++ plugins/info-report.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = ``
 const templateButtons = [
-    {index: 1, urlButton: {displayText: '🔧 Laporkan di sini', url: 'https://wa.me/6288279268363'}},
+    {index: 1, urlButton: {displayText: '🔧 Laporkan di sini', url: 'https://wa.me/6289516353968'}},
 ]
 let tm = {
 text: text,
