d308bcb4f57c4f4b378821dc4469cf9041f8adcd
malgtps12 <93052305+malgtps12@users.noreply.github.com>
2022-11-06 15:30:41 +0700

    Update mode-bot.js

--- plugins/mode-bot.js
+++ plugins/mode-bot.js
@@ -19,7 +19,7 @@ contextInfo: { externalAdReply :{
                         title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
                         body: `Subscribe YT My Bestie`,          previewType: 0,
                         thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
-                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+                        sourceUrl: '-'
                       }}
 })
 }
